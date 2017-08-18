14cbc7ff743ea02e08fc2cdf4ec9e61282eee7f5
Stefan Mitev <mr.mitew@gmail.com>
2017-08-18 12:46:18 +0200

    [add] Butterknife binding

--- framework/src/main/java/com/github/mrmitew/skeleton/welcome/view/WelcomeActivity.java
+++ framework/src/main/java/com/github/mrmitew/skeleton/welcome/view/WelcomeActivity.java
@@ -1,17 +1,25 @@
 package com.github.mrmitew.skeleton.welcome.view;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.github.mrmitew.skeleton.R;
 import com.github.mrmitew.skeleton.base.view.BaseActivity;
 import com.github.mrmitew.skeleton.di.activity.HasActivitySubcomponentBuilders;
 import com.github.mrmitew.skeleton.welcome.di.WelcomeActivityComponent;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class WelcomeActivity extends BaseActivity {
+    @BindView(R.id.tv_welcome_message)
+    TextView mTvWelcomeMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
     }
 
     @Override
